d6180366d54d62eab2584664cb4e0300267e3aa9
Enesozdogan <72387932+Enesozdogan@users.noreply.github.com>
2022-12-15 19:43:21 +0300

    1

--- Assets/Scripts/Grappling.cs
+++ Assets/Scripts/Grappling.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Grappling : MonoBehaviour
 {
+    //[SerializeField] Transform target;
+    [SerializeField] LineRenderer lr;
+    [SerializeField] DistanceJoint2D dj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dj.enabled=false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    if (Input.GetMouseButtonDown(0))
+    {   
+     RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+ 
+    if(hit.collider != null)
+    {
+        Debug.Log("object clicked: "+hit.collider.tag);
+    }
+
+    }
+    }
+
+    void HookOn(){
+        dj.enabled=true;
+        lr.positionCount=2;
+        //lr.SetPosition(0,)
     }
 }
